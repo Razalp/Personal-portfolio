a5e058cf2c7f4013f679136f8e0a414127ce3906
Razalp <razalp0012300@gmail.com>
2024-03-29 16:52:50 +0530

    change all

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -10,9 +10,9 @@ import instagramIcon from '../../assets/instagram.svg'
 export function Footer() {
   return (
     <Container className="footer">
-      <a href="https://vinayaksingh.in" className="logo">
-        <span>www.vinayak</span>
-        <span>singh.in</span>
+      <a href="" className="logo">
+        <span>Razal</span>
+     
       </a>
       <div>
         <p>
@@ -22,35 +22,29 @@ export function Footer() {
       </div>
       <div className="social-media">
         <a
-          href="https://www.linkedin.com/in/codevinayak"
+          href="https://www.linkedin.com/in/razalp/"
           target="_blank"
           rel="noreferrer"
         >
           <img src={linkedin} alt="Linkedin" />
         </a>
         <a
-          href="https://github.com/CodeVinayak/"
+          href="https://github.com/Razalp"
           target="_blank"
           rel="noreferrer"
         >
           <img src={githubIcon} alt="GitHub" />
         </a>
         <a
-          href="https://api.whatsapp.com/send/?phone=%2B919630576848&text=Hello+Vinayak"
+                href="https://api.whatsapp.com/send/?phone=%2B918129933192&text=Hello+Razal"
           target="_blank"
           rel="noreferrer"
         >
           <img src={whatsapp} alt="Whatsapp" />
         </a>
+       
         <a
-          href="https://t.me/CodeVinayak"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={telegram} alt="telegram" />
-        </a>
-        <a
-          href="https://www.instagram.com/vinayaksingh.in"
+            href="https://www.instagram.com/razal_____p/?next=%2F"
           target="_blank"
           rel="noreferrer"
         >
